d2f405a28ba3df545543eebf5f7ecbe53f6a72b9
DESKTOP-JTOCPO8\POORANI <pooranists@gmail.com>
2018-08-04 16:13:06 +0530

    First comit of all files - Excluding video

---
+++ Assets/Script/Camera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour {
+
+    [SerializeField]
+    Transform target;
+    Vector3 offset;
+	// Use this for initialization
+	void Start () {
+
+       offset = target.transform.position - transform.position;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 pos = target.transform.position - offset;
+        this.transform.position = Vector3.Lerp(this.transform.position, pos, 1.5f);
+
+	}
+}
---
+++ Assets/Script/Player.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    private Rigidbody rb;
+    [SerializeField]
+    private float speed;
+
+    private Animator anim;
+
+    float lengthinZaxis = 7.6f;
+    Vector3 lastposition;
+
+    [SerializeField]
+    GameObject platform;
+
+    [SerializeField]
+    Transform firstobject;
+
+    //[SerializeField]
+    //MeshRenderer platformmesh;
+
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+        rb = this.GetComponent<Rigidbody>();
+        rb.velocity = new Vector3(0f, 0f, speed);
+        lastposition = firstobject.transform.position;
+        //Debug.Log(platformmesh.bounds.size);
+
+        InvokeRepeating("Spawning",0f, 0.3f);
+
+	}
+	
+    private void Spawning()
+    {
+        GameObject _object = Instantiate(platform) as GameObject;
+        _object.transform.position = lastposition + new Vector3(0f, 0f, lengthinZaxis);
+        lastposition = _object.transform.position;
+
+    }
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetMouseButtonDown(0))
+        {
+            rb.AddForce(0f,07, 0f,ForceMode.Impulse);
+            anim.Play("Jumping");
+        }
+		
+	}
+}
